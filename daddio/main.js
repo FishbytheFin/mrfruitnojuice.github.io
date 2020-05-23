64b1321dfc72000c94b2a93bd69b8061dcd360ad
MrFruitNoJuice <62949492+MrFruitNoJuice@users.noreply.github.com>
2020-05-23 16:38:34 -0700

    Add files via upload

---
+++ daddio/main.js
@@ -0,0 +1,22 @@
+let slide_num = 1;
+function next(){
+	slide_num += 1;
+	if (slide_num == 2) {
+		document.getElementById('header0').innerHTML = "We Love you because you are the best"
+	}
+	if (slide_num == 3) {
+		document.getElementById('header0').innerHTML = "We Love you because you support us"
+	}
+	if (slide_num == 4) {
+		document.getElementById('header0').innerHTML = "We Love you because you care about us"
+	}
+	if (slide_num == 5) {
+		document.getElementById('header0').innerHTML = "We Love you because you invest in us"
+	}
+	if (slide_num == 6) {
+		document.getElementById('header0').innerHTML = "And we Love you because you love us!"
+	}
+	if (slide_num == 7) {
+		window.location.href = "http://m.youtube.com/";
+	}
+}
