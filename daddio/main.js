87b909ae7dce515906db1409126ac8a2da4c633f
MrFruitNoJuice <62949492+MrFruitNoJuice@users.noreply.github.com>
2020-05-29 19:02:31 -0700

    Update main.js

--- daddio/main.js
+++ daddio/main.js
@@ -2,16 +2,16 @@ let slide_num = 1;
 function next(){
 	slide_num += 1;
 	if (slide_num == 2) {
-		document.getElementById('header0').innerHTML = "We Love you because you are the best"
+		document.getElementById('header0').innerHTML = "We Love you because you are the best..."
 	}
 	if (slide_num == 3) {
-		document.getElementById('header0').innerHTML = "We Love you because you support us"
+		document.getElementById('header0').innerHTML = "We Love you because you support us..."
 	}
 	if (slide_num == 4) {
-		document.getElementById('header0').innerHTML = "We Love you because you care about us"
+		document.getElementById('header0').innerHTML = "We Love you because you care about us..."
 	}
 	if (slide_num == 5) {
-		document.getElementById('header0').innerHTML = "We Love you because you invest in us"
+		document.getElementById('header0').innerHTML = "We Love you because you invest in us..."
 	}
 	if (slide_num == 6) {
 		document.getElementById('header0').innerHTML = "And we Love you because you love us!"
