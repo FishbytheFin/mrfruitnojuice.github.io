c2bd143b0b503c3f9214a933bc9169696af68b43
MrFruitNoJuice <62949492+MrFruitNoJuice@users.noreply.github.com>
2020-06-19 20:22:55 -0700

    Add files via upload

--- daddio/main.js
+++ daddio/main.js
@@ -2,16 +2,16 @@ let slide_num = 1;
 function next(){
 	slide_num += 1;
 	if (slide_num == 2) {
-		document.getElementById('header0').innerHTML = "We Love you because you are the best"
+		document.getElementById('header0').innerHTML = "We Love you because you are the best..."
 	}
 	if (slide_num == 3) {
-		document.getElementById('header0').innerHTML = "We Love you because you support us"
+		document.getElementById('header0').innerHTML = "We Love you because you support us..."
 	}
 	if (slide_num == 4) {
-		document.getElementById('header0').innerHTML = "We Love you because you care about us"
+		document.getElementById('header0').innerHTML = "We Love you because you care about us..."
 	}
 	if (slide_num == 5) {
-		document.getElementById('header0').innerHTML = "We Love you because you invest in us"
+		document.getElementById('header0').innerHTML = "We Love you because you invest in us..."
 	}
 	if (slide_num == 6) {
 		document.getElementById('header0').innerHTML = "And we Love you because you love us!"
@@ -19,5 +19,9 @@ function next(){
 	if (slide_num == 7) {
 		document.getElementById('header0').innerHTML = ""
 		document.getElementById('heart_img').setAttribute("src", "images/giphy (2).gif")
+	}
+    if (slide_num == 8) {
+		document.getElementById('header0').innerHTML = "P.S. By \"We\", I mean me, Finn, your son. I did all the work."
+		document.getElementById('heart_img').setAttribute("src", "")
 	}
 }
